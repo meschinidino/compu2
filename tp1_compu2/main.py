642793a1e934f5cd27f5c78a83aa05c2f55136c4
meschinidino <francodmsoloing@gmail.com>
2024-08-05 19:53:04 -0300

    TP 1 finalizado

---
+++ tp1_compu2/main.py
@@ -0,0 +1,8 @@
+from functions import process_image
+
+# Parameters
+image_path = 'image.jpeg'  # Update with your image path
+n = 4  # Number of parts, can be adjusted based on requirement
+
+# Process the image
+process_image(image_path, n)
